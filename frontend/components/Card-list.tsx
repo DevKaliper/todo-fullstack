c3066823b78b3220c9a31f76e1524677235a82d3
Dev Kaliper <gxlthz@gmail.com>
2023-11-20 20:41:32 -0500

    card fixed

--- frontend/components/Card-list.tsx
+++ frontend/components/Card-list.tsx
@@ -14,18 +14,23 @@ export function CardWithForm() {
   return (
     <Card className="w-[350px]">
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>TODO ID: ID</CardTitle>
+        <CardDescription>Created on: march 14, 2034</CardDescription>
       </CardHeader>
       <CardContent className="text-center">
-        <h3 className="my-5 font-bold text-2xl">Nombre de la lista</h3>
+        <h3 className="mb-5 font-bold text-2xl">Nombre de la lista</h3>
+
+        <p className="mb-5">
+          <strong>Descripción:</strong> Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Quisquam, voluptates.
+        </p>
 
         <h3>Estado de la lista: <strong className="italic">done</strong></h3>
        
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
+        <Button variant="destructive" >Delete</Button>
+        <Button >Update</Button>
       </CardFooter>
     </Card>
   )
