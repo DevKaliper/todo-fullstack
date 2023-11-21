194fd4b39aca3b27721ad3c82259b880ce63609c
Dev Kaliper <gxlthz@gmail.com>
2023-11-20 20:38:51 -0500

    description added

--- frontend/components/AddNew.tsx
+++ frontend/components/AddNew.tsx
@@ -1,7 +1,13 @@
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select"
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 import {
   Sheet,
   SheetClose,
@@ -11,7 +17,8 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
+} from "@/components/ui/sheet";
+import { Textarea } from "@/components/ui/textarea";
 
 export function AddNew() {
   return (
@@ -22,33 +29,53 @@ export function AddNew() {
       <SheetContent>
         <SheetHeader>
           <SheetTitle>Add New Todo</SheetTitle>
-          <SheetDescription>
-            Add a new todo to your list.
-          </SheetDescription>
+          <SheetDescription>Add a new todo to your list.</SheetDescription>
         </SheetHeader>
         <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
+            <Label
+              htmlFor="name"
+              className="text-right">
               Name
             </Label>
-            <Input id="name" value="" className="col-span-3" placeholder="Do something..." />
+            <Input
+              id="name"
+              value=""
+              className="col-span-3"
+              placeholder="Do something..."
+            />
           </div>
           <div className="grid grid-cols-4 items-center gap-4">
-          
-              <Label className="text-right" htmlFor="framework">Estatus</Label>
-              <Select>
-                <SelectTrigger className="col-span-3" id="framework">
-                  <SelectValue placeholder={"✅"} />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="done">done</SelectItem>
-                  <SelectItem value="pending">pending</SelectItem>
-               
-                </SelectContent>
-              </Select>
-            </div>
-            
-            </div>
+            <Label
+              className="text-right"
+              htmlFor="Description">
+              Description
+            </Label>
+            <Textarea
+              id="Description"
+              className="col-span-3"
+              placeholder="Do something..."
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label
+              className="text-right"
+              htmlFor="Estatus">
+              Estatus
+            </Label>
+            <Select>
+              <SelectTrigger
+                className="col-span-3"
+                id="Estatus">
+                <SelectValue placeholder={"✅"} />
+              </SelectTrigger>
+              <SelectContent position="popper">
+                <SelectItem value="done">done</SelectItem>
+                <SelectItem value="pending">pending</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+        </div>
         <SheetFooter>
           <SheetClose asChild>
             <Button type="submit">Save</Button>
@@ -56,5 +83,5 @@ export function AddNew() {
         </SheetFooter>
       </SheetContent>
     </Sheet>
-  )
+  );
 }
--- frontend/components/Card-list.tsx
+++ frontend/components/Card-list.tsx
@@ -9,15 +9,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
 
 export function CardWithForm() {
   return (
@@ -26,29 +17,11 @@ export function CardWithForm() {
         <CardTitle>Create project</CardTitle>
         <CardDescription>Deploy your new project in one-click.</CardDescription>
       </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
+      <CardContent className="text-center">
+        <h3 className="my-5 font-bold text-2xl">Nombre de la lista</h3>
+
+        <h3>Estado de la lista: <strong className="italic">done</strong></h3>
+       
       </CardContent>
       <CardFooter className="flex justify-between">
         <Button variant="outline">Cancel</Button>
