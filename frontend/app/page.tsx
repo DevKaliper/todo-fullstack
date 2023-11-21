31585bb3dc0a0782fe0245a4277a0e67c6e99f2e
Dev Kaliper <gxlthz@gmail.com>
2023-11-20 20:07:53 -0500

    Add TodoSection component to Home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,9 @@
-
+import TodoSection from "../components/todo-section";
 export default function Home() {
 return(
 <section>
   <h1 className="text-center my-3">HOLA MUNDO</h1>
+  <TodoSection/>
 </section>
 )
  
---
+++ frontend/components/todo-section.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+const TodoSection = () => {
+  return (
+    <div className="flex justify-center items-center  ">
+      <div
+        className=" min-h-[80vh] grid 2xl:grid-cols-5 place-content-center gap-6">
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+        <div className="h-60 w-60 border border-red-300"></div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoSection;
