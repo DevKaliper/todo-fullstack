29be52af680e30af3a2e5408964272a022f25a99
Dev Kaliper <gxlthz@gmail.com>
2023-11-20 20:48:38 -0500

    fronted maquetado

--- frontend/components/Card-list.tsx
+++ frontend/components/Card-list.tsx
@@ -1,6 +1,6 @@
-import * as React from "react"
+import * as React from "react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,7 +8,21 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Textarea } from "./ui/textarea";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 
 export function CardWithForm() {
   return (
@@ -25,13 +39,66 @@ export function CardWithForm() {
           adipisicing elit. Quisquam, voluptates.
         </p>
 
-        <h3>Estado de la lista: <strong className="italic">done</strong></h3>
-       
+        <h3>
+          Estado de la lista: <strong className="italic">done</strong>
+        </h3>
       </CardContent>
       <CardFooter className="flex justify-between">
-        <Button variant="destructive" >Delete</Button>
-        <Button >Update</Button>
+        <Button variant="destructive">Delete</Button>
+        <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>TODO # Id</DialogTitle>
+          <DialogDescription>
+            Edit your TODO list.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue={"Su valor normal"}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="Description" className="text-right">
+              Description
+            </Label>
+            <Textarea
+              id="Description"
+              defaultValue={"Su valor normal"}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="status" className="text-right">
+              Status
+            </Label>
+            <Select>
+              <SelectTrigger>
+                <SelectValue>Done</SelectValue>
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="done">Done</SelectItem>
+                <SelectItem value="pending">pending</SelectItem>
+              </SelectContent>
+            </Select>
+        </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
       </CardFooter>
     </Card>
-  )
+
+  );
 }
